157e93ed57297aa45967206aa9466bd737642272
paraselene92 <paraselene92@gmail.com>
2019-07-02 00:32:15 +0900

    delete create_db, add resources:

--- handler.py
+++ handler.py
@@ -8,32 +8,7 @@ def hello(event, context):
     for entry in d["entries"]:
         feed_links.insert(0, entry.link)
 
-    create_db()
-
     return feed_links
-
-def create_db():
-    db = boto3.client("dynamodb")
-    db.create_table(
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'url',
-                'AttributeType': 'S',
-            },
-        ],
-        TableName='testdb',
-        KeySchema=[
-            {
-                'AttributeName': 'url',
-                'KeyType': 'HASH',
-            },
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 123,
-            'WriteCapacityUnits': 123
-        }
-    )
-
 
     # Use this code if you don't use the http event with the LAMBDA-PROXY
     # integration
